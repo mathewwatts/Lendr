ab8f4ebc56d82ab57337365cd30cdb5021767bc7
Mathew Watts <46589151+mathewwatts@users.noreply.github.com>
2019-03-27 17:36:30 +1100

    Add files via upload

---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create(name: 'Kids')
+Category.create(name: 'Home')
+Category.create(name: 'Outdoor')
+Category.create(name: 'Fashion')
+Category.create(name: 'Events')
+Category.create(name: 'Recreation')
+Category.create(name: 'Office')
+Category.create(name: 'Electronics')
+Category.create(name: 'Miscellaneous')
